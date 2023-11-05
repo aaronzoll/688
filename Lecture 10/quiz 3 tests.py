f43ba7d41f936da401b22d5cb0f09f5c797e1404
aaronzoll <aaronzoll360@gmail.com>
2023-11-05 12:18:16 -0500

    november dump

--- Lecture 10/quiz 3 tests.py
+++ Lecture 10/quiz 3 tests.py
@@ -1,4 +1,7 @@
-L=["help", "quizzes", "hard!"]
-M=map(lambda x : len(x)+3,L)
-next(M)
-print(next(M))
+class mylist(list):
+    def __init__(self):
+        self.list=[]
+x=mylist()
+x.append(9)
+x.list.append(10)
+print(x.list[0])
